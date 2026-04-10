f088fa2fe54557af238cc1f1b7584eeda9ca2e6b
SpencerMSU <rjdcbxxhcx@gmail.com>
2026-04-10 12:29:31 +0300

    четвертый коммит

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ class Main{
         System.out.println("Первый коммит");
         System.out.println("второй коммит");
         System.out.println("третий коммит");
+        System.out.println("чертивиртый коммит");
     }
 } 
